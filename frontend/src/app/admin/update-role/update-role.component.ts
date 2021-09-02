6343053e14e4fdd36f0291a03c977533067d353f
Anthony Ahumada <locopala99@gmail.com>
2021-09-02 14:19:26 +0200

    More js

--- frontend/src/app/admin/update-role/update-role.component.ts
+++ frontend/src/app/admin/update-role/update-role.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { RoleService } from "../../services/role.service";
+import { RoleService } from '../../services/role.service';
 @Component({
   selector: 'app-update-role',
   templateUrl: './update-role.component.html',
-  styleUrls: ['./update-role.component.css']
+  styleUrls: ['./update-role.component.css'],
 })
 export class UpdateRoleComponent implements OnInit {
-  public roleData:any;
-  public registerData:any;
-  constructor(private _roleService:RoleService) { }
-
-  ngOnInit(): void {
+  public roleData: any;
+  public registerData: any;
+  constructor(private _roleService: RoleService) {
     this.roleData = {};
-    this.registerData={}
+    this.registerData = {};
   }
 
-  
-
+  ngOnInit(): void {
+    this._roleService.listRole().subscribe((res) => {
+      this.roleData = res.user;
+      console.log(this.roleData);
+    });
+  }
 }
