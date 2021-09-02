7a10447f4f26d19ea240739b474f4fa6a3d3ecf1
Anthony Ahumada <locopala99@gmail.com>
2021-09-02 13:28:35 +0200

    more js

--- frontend/src/app/admin/list-role/list-role.component.ts
+++ frontend/src/app/admin/list-role/list-role.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RoleService } from '../../services/role.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-list-role',
@@ -6,10 +16,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-role.component.css']
 })
 export class ListRoleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  displayedColumns: string[] = ['NAME', 'EMAIL', 'ACTIONS'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort();
+  roleData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
+  constructor(private _snackBar: MatSnackBar , private roleService: RoleService) { 
+    this.roleData = {};
+    this.dataSource = new MatTableDataSource(this.roleData)
   }
 
+  ngOnInit(): void {
+    this.roleService.listRole().subscribe(
+      (res) => {
+        this.roleData = res.role;
+        
+        console.log(this.roleData)
+        this.dataSource = new MatTableDataSource(this.roleData);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        
+      },
+      (err) => {
+        this.message = err.error;
+        this.openSnackBarError();
+      }
+    );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
 }
